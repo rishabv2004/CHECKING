fe8e54ec4e7f3d0c079ab5ec62b4484ae7bfc85e
Rishab Verma <rishabhv894@gmail.com>
2025-01-19 16:40:01 +0530

    Delete CCSU TUTOR directory

--- CCSU TUTOR/Script/js.js
+++
@@ -1,8 +0,0 @@
-// Scroll to the top of the page smoothly
-function goToTop() {
-    window.scrollTo({
-      top: 0,
-      behavior: 'smooth'
-    });
-  }
-  
--- CCSU TUTOR/Script/responsive-home.js
+++
@@ -1,7 +0,0 @@
-// script.js
-const hamburger = document.getElementById('hamburger');
-const navLinks = document.getElementById('nav-links');
-
-hamburger.addEventListener('click', () => {
-    navLinks.classList.toggle('active');
-});
